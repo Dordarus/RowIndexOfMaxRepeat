e2513465ea1d7781c827958728cfead6163b0bf7
Daniel Vysotskyi <dordarus@gmail.com>
2017-01-06 21:16:27 +0200

    Finished

--- RowIndexOfMaxRepeat/Program.cs
+++ RowIndexOfMaxRepeat/Program.cs
@@ -1,15 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace RowIndexOfMaxRepeat
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
+            int[,] a =
+            {
+                {1, 3, 6, 1, 1, 6}, //3
+                {1, 2, 1, 4, 5, 6}, //5
+                {1, 2, 3, 2, 5, 1}, //4
+                {1, 2, 1, 4, 2, 6}, //4
+                {1, 5, 5, 4, 5, 1}  //3
+            };
+
+            var obj = new object[a.GetLength(0)];
+            var s = new int[a.GetLength(0)];
+
+            for (var i = 0; i < a.GetLength(0); i++)
+            {
+                var sb = new StringBuilder();
+                for (var j = 0; j < a.GetLength(1); j++)
+                {
+                    sb.Append(a[i, j] + "*");
+                }
+                obj[i] = sb.ToString().TrimEnd('*').Split('*');
+
+                s[i] = ((string[])obj[i]).Distinct().Count();
+            }
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (s[i] == s.Min())
+                {
+                    Console.WriteLine($"Max repetition in {i + 1}-th row");
+                }
+            }
         }
     }
 }
